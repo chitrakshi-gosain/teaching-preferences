ec17b9e6b7d716ea67f6da3e6f5d2a059d47588b
Chitrakshi <chitrakshigosain@gmail.com>
2024-05-24 16:12:00 +1000

    added comments

--- timetable scraper/timetable_scraper.py
+++ timetable scraper/timetable_scraper.py
@@ -11,12 +11,15 @@ class TimetableScraper:
         """
         Fetch timetable data for the given course code and term
         """
+        # Fetch page content
         url = f'https://timetable.unsw.edu.au/2024/{self.course_code}.html'
         response = requests.get(url)
         response.raise_for_status()
 
+        # Parse page content
         html_soup = BeautifulSoup(response.content, 'html.parser')
 
+        # Find <td> cells of class 'formBody'
         form_body_cells = html_soup.find_all('td', class_='formBody')
         if not form_body_cells:
             print("No td tags found with class 'formBody'")
@@ -24,12 +27,14 @@ class TimetableScraper:
 
         classes = []
         for cell in form_body_cells:
+            # Find cells which have class data
             inner_cell = cell.find_all('td', class_='formBody', colspan='6')
+
+            # Check if class is of type 'Laboratory'
             if len(inner_cell) == 1 and cell.find('td', class_='data', string='Laboratory'):
-                class_details = self._extract_class_details(inner_cell[0])
-                current_term = cell.find('table').find_all('tr')[1].find_all('td', class_='data')[2].text.split()[0]
-                if current_term == self.term:
-                    classes.append(class_details)
+                # Checking if the class belongs to the specified term
+                if self.term == cell.find('table').find_all('tr')[1].find_all('td', class_='data')[2].text.split()[0]:
+                    classes.append(self._extract_class_details(inner_cell[0]))
 
         return pd.DataFrame(classes)
 
@@ -50,9 +55,14 @@ class TimetableScraper:
         df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
+    # Prompt user to input course code and term
     course_code = input('Enter the course code: ')
     term = input('Enter the term: ')
+
+    # Create scraper instance and fetch timetable data
     scraper = TimetableScraper(course_code, term)
     timetable_data = scraper.fetch_timetable()
+
+    # Save timetable data to CSV if it's not empty
     if not timetable_data.empty:
         scraper.save_timetable_to_csv(timetable_data)
